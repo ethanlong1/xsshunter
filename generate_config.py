4902c3942d61752dd836a9dabb009e83c9c78ed9
ethanlong1 <56567531+ethanlong1@users.noreply.github.com>
2020-02-26 23:37:15 +1100

    Update generate_config.py

--- generate_config.py
+++ generate_config.py
@@ -115,7 +115,7 @@ print """
 
 print "What is the base domain name you will be using? "
 print "(ex. localhost, www.example.com)"
-hostname = os.environ['d_domain']
+hostname = os.environ['domain']
 if hostname != "":
 	settings["domain"] = hostname
 nginx_template = nginx_template.replace( "fakedomain.com", settings["domain"] )
@@ -124,17 +124,17 @@ print "Great! Now let's setup your Mailgun account to send XSS alerts to."
 print ""
 print "Enter your API key: "
 print "(ex. key-8da843ff65205a61374b09b81ed0fa35)"
-settings["mailgun_api_key"] = os.environ['d_mail_api']
+settings["mailgun_api_key"] = os.environ['mail_api']
 
-settings["mailgun_sending_domain"] = os.environ['d_mail_domain']
+settings["mailgun_sending_domain"] = os.environ['mail_domain']
 print ""
 print "What email address is sending the payload fire emails?: "
 print "(ex. no-reply@example.com)"
-settings["email_from"] = os.environ['d_mail_email']
+settings["email_from"] = os.environ['mail_email']
 print ""
 print "Where should abuse/contact emails go?: "
 print "(ex. yourpersonal@gmail.com)"
-settings["abuse_email"] = os.environ['d_mail_spam']
+settings["abuse_email"] = os.environ['mail_spam_email']
 print ""
 print ""
 print "What postgres user is this service using? "
@@ -143,7 +143,7 @@ settings["postgreql_username"] = "xsshunter"
 print ""
 print "What is the postgres user's password? "
 print "(ex. @!$%@^%UOFGJOEJG$)"
-settings["postgreql_password"] = os.environ['d_psql_p']
+settings["postgreql_password"] = os.environ['password']
 print ""
 print "What is the postgres user's DB? "
 print "(ex. xsshunter)"
@@ -161,16 +161,3 @@ print "Minting new nginx configuration file..."
 file_handler = open( "default", "w" )
 file_handler.write( nginx_template )
 file_handler.close()
-
-print """
-Setup complete! Please now copy the 'default' file to /etc/nginx/sites-enabled/default
-This can be done by running the following:
-sudo cp default /etc/nginx/sites-enabled/default
-
-Also, please ensure your wildcard SSL certificate and key are available at the following locations:
-/etc/nginx/ssl/""" + hostname + """.crt; # Wildcard SSL certificate
-/etc/nginx/ssl/""" + hostname + """.key; # Wildcard SSL key
-
-Good luck hunting for XSS!
-							-mandatory
-"""
