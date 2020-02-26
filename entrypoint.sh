5d5ed3224b8ac9b0dc7791b551ed2fbdd8bcc06a
ethanlong1 <56567531+ethanlong1@users.noreply.github.com>
2020-02-27 10:07:15 +1100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,6 @@ service postgresql start
 su - postgres -c "psql -d template1 -c 'CREATE USER xsshunter WITH PASSWORD '\''${password}'\'';'"
 su - postgres -c "psql -d template1 -c 'CREATE DATABASE xsshunter;'"
 /usr/bin/python2.7 /xsshunter/generate_config.py
-cp cert_crt /etc/nginx/ssl/${domain}.crt
-cp cret_key /etc/nginx/ssl/${domain}.key
-#nohup sh /xsshunter/gui/guiserver.sh
-#nohup sh /xsshunter/api/apiserver.py
+sleep 20 &&  cp /xsshunter/ssl.crt /etc/nginx/ssl/${domain}.crt ; cp /xsshunter/ssl.key /etc/nginx/ssl/${domain}.key
+nohup sh /xsshunter/gui/guiserver.sh
+nohup sh /xsshunter/api/apiserver.py
