26067f675959bdaa0b11fd399033e756fca7ce06
ethanlong1 <56567531+ethanlong1@users.noreply.github.com>
2020-02-27 12:52:23 +1100

    Update guiserver.sh

--- gui/guiserver.sh
+++ gui/guiserver.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cd xsshunter/gui/ && virtualenv env %% . env/bin/activate %% pip install -r requirements.txt %% ./guiserver.py
+cd /xsshunter/gui/ && virtualenv env %% . env/bin/activate %% pip install -r requirements.txt %% ./guiserver.py
