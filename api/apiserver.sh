6e9ff9ec089f3202fc6b539f3fb868fc73133d3d
ethanlong1 <56567531+ethanlong1@users.noreply.github.com>
2020-02-27 12:58:18 +1100

    Update apiserver.sh

--- api/apiserver.sh
+++ api/apiserver.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cd xsshunter/api/ && virtualenv env && . env/bin/activate && pip install -r requirements.txt && /usr/bin/python2.7 ./apiserver.py
+cd xsshunter/api/ && virtualenv env && . env/bin/activate && pip install -r requirements.txt && ./apiserver.py
