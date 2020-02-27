717be3b5b3c4e59fef051c4def66a46f0df07ff6
ethanlong1 <56567531+ethanlong1@users.noreply.github.com>
2020-02-27 13:07:44 +1100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,5 +7,6 @@ su - postgres -c "psql -d template1 -c 'CREATE DATABASE xsshunter;'"
 mkdir /etc/nginx/ssl/
 echo "$crt" | base64 -d > /etc/nginx/ssl/${domain}.crt 
 echo "$key" | base64 -d > /etc/nginx/ssl/${domain}.key
+nohup sh /xsshunter/api/apiserver.sh
+sleep 10
 nohup sh /xsshunter/gui/guiserver.sh
-nohup sh /xsshunter/api/apiserver.py
