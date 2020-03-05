ba359c804d6ea9a0a5091546aa5d41caae1d5a4c
ethanlong1 <56567531+ethanlong1@users.noreply.github.com>
2020-03-05 13:56:30 +1100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,4 +9,5 @@ mkdir /etc/nginx/ssl/
 echo "$crt" | base64 -d > /etc/nginx/ssl/${domain}.crt 
 echo "$key" | base64 -d > /etc/nginx/ssl/${domain}.key
 service nginx restart
+mkdir /xsshunter/api/uploads
 sleep 10 && nohup sh /xsshunter/api/apiserver.sh | nohup sh /xsshunter/gui/guiserver.sh
